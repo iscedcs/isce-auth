21843746ac7dfbbaee357f24dd7da236b0bd60b8
ozigbohub <eoziegbe2@gmail.com>
2024-09-30 12:43:34 +0100

    changing api to docs

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   .addTag('ISCE')
   .build();
 const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
+SwaggerModule.setup('docs', app, document);
 
   await app.listen(3223);
 }
