0a76be91016b71894810c077cc0248a745238e4e
ozigbohub <eoziegbe2@gmail.com>
2025-03-19 11:08:35 +0100

    port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cors from 'cors'; 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -37,9 +38,11 @@ async function bootstrap() {
       'JWT-auth' // The name used to reference this auth method in the Swagger UI
     )
     .build();
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('docs', app, document);
+ const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3223);
+ const PORT = process.env.PORT || 3223;  // Ensure it uses the correct port
+  app.enableCors();  // If CORS issues
+await app.listen(PORT);
 }
 bootstrap();
